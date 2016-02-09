1a8a8dddbb9b083c462c68491b98a65e8d941fb7
Abigail Rolling and Matt Rosanio <arolling@gmail.com>
2016-02-09 08:58:24 -0800

    add full letter testing functionallity

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -21,10 +21,28 @@ public class Scrabble {
 
   public Integer letterScore(char letter) {
     char[] score4 = {'f','h','v','w','y'};
+    char[] score3 = {'b','c','m','p'};
     for(char scoreLetter: score4) {
       if(letter == scoreLetter) {
         return 4;
       }
+    }
+    for(char scoreLetter: score3) {
+      if(letter == scoreLetter) {
+        return 3;
+      }
+    }
+    if(letter == 'd' || letter == 'g') {
+      return 2;
+    }
+    if(letter == 'k') {
+      return 5;
+    }
+    if(letter == 'j' || letter == 'x') {
+      return 8;
+    }
+    if(letter == 'q' || letter == 'z') {
+      return 10;
     }
     return 1;
   }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -13,16 +13,44 @@ public class ScrabbleTest {
   }
 
   @Test
-  public void letterScore_UserEntersH_Returns4() {
+  public void letterScore_UserEntersF_Returns4() {
     Scrabble testApp = new Scrabble();
     Integer score = 4;
     assertEquals(score, testApp.letterScore('f'));
   }
 
   @Test
-  public void letterScore_UserEntersH_Returns4() {
+  public void letterScore_UserEntersC_Returns3() {
     Scrabble testApp = new Scrabble();
     Integer score = 3;
     assertEquals(score, testApp.letterScore('c'));
+  }
+
+  @Test
+  public void letterScore_UserEntersD_Returns2() {
+    Scrabble testApp = new Scrabble();
+    Integer score = 2;
+    assertEquals(score, testApp.letterScore('d'));
+  }
+
+  @Test
+  public void letterScore_UserEntersK_Returns5() {
+    Scrabble testApp = new Scrabble();
+    Integer score = 5;
+    assertEquals(score, testApp.letterScore('k'));
+  }
+
+  @Test
+  public void letterScore_UserEntersJ_Returns8() {
+    Scrabble testApp = new Scrabble();
+    Integer score = 8;
+    assertEquals(score, testApp.letterScore('j'));
+  }
+
+  @Test
+  public void letterScore_UserEntersZ_Returns10() {
+    Scrabble testApp = new Scrabble();
+    Integer score = 10;
+    assertEquals(score, testApp.letterScore('z'));
   }
 }
